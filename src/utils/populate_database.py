732b768faad858f7cc646d014e48c956c19a80f8
krenbergs <kasper.renhornberg@gmail.com>
2024-12-05 18:19:01 +0100

    fix: popdb

--- src/utils/populate_database.py
+++ src/utils/populate_database.py
@@ -11,8 +11,8 @@ session = Session()
 
 def populate_db():
     # Add Accounts
-    account1 = AccountTable(phone_number="467012345689", subscribed=False)
-    account2 = AccountTable(phone_number="467312456789", subscribed=True)
+    account1 = AccountTable(gmail="hamliannu@gmail.com")
+    account2 = AccountTable(gmail="")
 
     session.add_all([account1, account2])
     session.commit()
